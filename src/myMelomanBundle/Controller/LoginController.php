ebceff385e4e9a217d584147012b233a2338199a
Alexbeis <alexbeis09@gmail.com>
2017-05-11 23:03:46 +0200

    Refactoring loginController

--- src/myMelomanBundle/Controller/LoginController.php
+++ src/myMelomanBundle/Controller/LoginController.php
@@ -4,81 +4,32 @@ namespace myMelomanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Google_Client;
-use Google_Service_Oauth2;
-use Symfony\Component\HttpFoundation\Session\Session;
+use myDomain\DTO\LoginDTO;
 
 class LoginController extends Controller
 {
-    private $client;
-    private $service;
-    private $session;
-    private $userInfo;
-    private $em;
-
-    public function __construct()
-    {
-        $this->client = new Google_Client();
-        $this->session = new Session(); // Force to create a session
-    }
-
 
     public function loginAction(Request $request)
     {
-
-        $authUrl = null;
-        //$this->session = $request->getSession();
-        $this->googleInit();
-        $authUrl = $this->client->createAuthUrl();
+        $loginDTO = new LoginDTO(null, $request->getSession());
+        $loginUseCase = $this->get('app.application.usecases.login.login');
+        $returnLoginDTO = $loginUseCase->execute($loginDTO);
 
         return $this->render('loginView/googleLogin.html.twig',
-            array('authUrl' => $authUrl )
+            array('authUrl' => $returnLoginDTO->getGoogleURL())
         );
 
     }
 
     public function validateAction(Request $request)
     {
-        if ($this->session == null) {
-            $this->session->start();
-        }
-        $this->googleInit();
-
-        if ($request->get('code')) {
-            $this->client->authenticate($request->get('code'));
-            $this->session->set('access_token',$this->client->getAccessToken());
-            header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
-        }
-
-        if ($this->session->get('access_token')) {
-            $this->client->setAccessToken($this->session->get('access_token'));
-        }
-
-        if ($this->client->getAccessToken()) {
-            $this->userinfo = $this->service->userinfo->get();
-            $this->session->set('familyName', $this->userinfo->getFamilyName());
-            $this->session->set('email', $this->userinfo->getEmail());
-
-        }
-
-        $createUserUseCase = $this->get('app.application.usecases.user.create');
-        $user = $createUserUseCase->execute($this->session->get('familyName'), $this->session->get('email'));
+        $code = $request->get('code');
+        $loginDTO = new LoginDTO($code, $request->getSession());
+        $loginUseCase = $this->get('app.application.usecases.login.login');
+        $returnLoginDTO = $loginUseCase->execute($loginDTO);
 
         return $this->redirectToRoute('home');
 
-
-    }
-
-
-    private function googleInit()
-    {
-        $this->client->setApplicationName("Melomaniacs");
-        $this->client->setClientId($this->getParameter('google_client_id'));
-        $this->client->setClientSecret($this->getParameter('google_secret_id'));
-        $this->client->setRedirectUri($this->getParameter('google_redirect_uri'));
-        $this->client->setScopes(array($this->getParameter('google_email_scope'),$this->getParameter('google_profile_scope')));
-        $this->service = new Google_Service_Oauth2($this->client);
 
     }
 
