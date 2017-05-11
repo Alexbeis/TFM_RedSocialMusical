4aff35e89d354dc6913da4b86e4804176ce40c1d
Alexbeis <alexbeis09@gmail.com>
2017-05-11 23:03:11 +0200

    Modifying LoginDTO

--- src/myDomain/DTO/LoginDTO.php
+++ src/myDomain/DTO/LoginDTO.php
@@ -2,15 +2,24 @@
 
 namespace myDomain\DTO;
 
+use Symfony\Component\HttpFoundation\Session\Session;
+
 class LoginDTO
 {
     private $statusCode;
     private $googleCode;
+    private $userName;
+    private $email;
+    /**
+     * @var Session
+     */
     private $session;
+    private $googleURL;
 
-    public function __construct($googleCode = '')
+    public function __construct($googleCode = null, $session)
     {
         $this->googleCode = $googleCode;
+        $this->session = $session;
     }
 
     /**
@@ -22,6 +31,14 @@ class LoginDTO
     }
 
     /**
+     * @param mixed $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
      * @return string
      */
     public function getGoogleCode()
@@ -29,5 +46,69 @@ class LoginDTO
         return $this->googleCode;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getGoogleURL()
+    {
+        return $this->googleURL;
+    }
+
+    /**
+     * @param mixed $googleURL
+     */
+    public function setGoogleURL($googleURL)
+    {
+        $this->googleURL = $googleURL;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    /**
+     * @param mixed $session
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
 
 }
