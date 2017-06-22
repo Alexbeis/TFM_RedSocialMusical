dd41cba6c1e452fccd44d3157432bdedad9cb553
Alexbeis <alexbeis09@gmail.com>
2017-06-22 17:48:06 +0200

    Spotify Web Api

---
+++ src/myDomain/UseCases/Spotify/SpotifyUseCase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace myDomain\UseCases\Spotify;
+
+use SpotifyWebAPI;
+
+class SpotifyUseCase
+{
+    private $spotifySession;
+    private $spotifyClientId;
+    private $spotifySecretId;
+    private $spotifyWebApi;
+
+    public function __construct($spotifyClientId, $spotifySecretId)
+    {
+        $this->spotifyClientId = $spotifyClientId;
+        $this->spotifySecretId = $spotifySecretId;
+        $this->spotifyWebApi   = new SpotifyWebAPI\SpotifyWebAPI();
+        $this->spotifySession  = new SpotifyWebAPI\Session($this->spotifyClientId,$this->spotifySecretId);
+
+
+        $this->spotifySession->requestCredentialsToken();
+        $accessToken = $this->spotifySession->getAccessToken();
+
+        $this->spotifyWebApi->setAccessToken($accessToken);
+        
+    }
+
+    public function searchSong($data)
+    {
+
+        $result =  (array)$this->spotifyWebApi->search($data['song'],array("track"),array('limit'=>5,'offset'=>0));
+        //  $uri = $result['tracks']->items[0]->uri;
+        return $result;
+
+    }
+
+}
---
+++ web/assets/js/spotify.js
@@ -0,0 +1,78 @@
+var SpotifyRequest = (function () {
+
+    var searchUrl = getBaseUrl() + '/spotify/search',
+        createUrl = getBaseUrl() + '/create-publication-spotify/new',
+        method    = 'POST';
+
+    return {
+        search: function (e) {
+             var song = document.getElementById('song').value;
+             var data = {song :song};
+             this.spotifyRequest(method , searchUrl, data);
+
+        },
+
+        share: function(e) {
+            // Pillar uri
+            //e.preventDefault();
+            var share = document.getElementById('share-button'),
+                shareLen = share.parentNode.parentNode.childNodes.length;
+
+            for (var i=0; i < shareLen; i++) {
+                if(share.parentNode.parentNode.childNodes[i].classList != undefined  && share.parentNode.parentNode.childNodes[i].classList.contains('spoti-content')) {
+                    for (var j=0; j < share.parentNode.parentNode.childNodes[i].childNodes.length; j++){
+                        if (share.parentNode.parentNode.childNodes[i].childNodes[j].tagName == 'INPUT' ) {
+                            var uri = share.parentNode.parentNode.childNodes[i].childNodes[j].getAttribute('data-uri');
+                        }
+                    }
+                }
+            }
+            console.log(uri);
+            var data = {uri: uri};
+            this.createPublicationRequest(method, createUrl, data);
+        },
+
+        spotifyRequest: function(method, url , data) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    console.log(xhr.responseText);
+                    // Pass to Pure javascript
+                    $('.spoti-content').html(JSON.parse(xhr.responseText).view);
+
+                }
+            };
+            xhr.open(method, url, false);
+            xhr.send(JSON.stringify(data));
+        },
+        createPublicationRequest: function (method, url, data) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    //console.log(xhr.responseText);
+                    //location.reload();
+                }
+            };
+            xhr.open(method, url, false);
+            xhr.send(JSON.stringify(data));
+
+        }
+
+    }
+}());
+
+//Spotify player responsive:
+$(document).ready(function(){
+    $('#spoti-iframe').each( function() {
+        $(this).css('width',$(this).parent(1).css('width'));
+        $(this).attr('src',$(this).attr('src'));
+    });
+});
+$(window).resize(function() {
+    $('#spoti-iframe').each( function() {
+        $(this).css('width',$(this).parent(1).css('width'));
+        $(this).attr('src',$(this).attr('src'));
+    });
+});
+
